c1478c5f8fa1c3e51b54c6705c6d349142dc3db0
IsaacMvmv <67015742+IsaacMvmv@users.noreply.github.com>
2022-07-07 23:58:53 +0200

    Update SetupMono.sh

--- Engine/Build/BatchFiles/Linux/SetupMono.sh
+++ Engine/Build/BatchFiles/Linux/SetupMono.sh
@@ -13,18 +13,4 @@ IS_MONO_INSTALLED=1
 IS_MS_BUILD_AVAILABLE=1
 MONO_VERSION_PATH=$(command -v mono) || true
 
-# Setup bundled Mono if cannot use installed one
-if [ $IS_MONO_INSTALLED -eq 0 ]; then
-	echo Setting up Mono
-	CUR_DIR=$PWD
-	export UE_MONO_DIR=$CUR_DIR/../../../Binaries/ThirdParty/Mono/Linux
-	export PATH=$UE_MONO_DIR/bin:$PATH
-	export MONO_PATH=$UE_MONO_DIR/lib/mono/4.5:$MONO_PATH
-	export MONO_CFG_DIR=$UE_MONO_DIR/etc
-	export LD_LIBRARY_PATH=$UE_MONO_DIR/$HOST_ARCH/lib:$LD_LIBRARY_PATH
-else
-	export IS_MONO_INSTALLED=$IS_MONO_INSTALLED
-	export IS_MS_BUILD_AVAILABLE=$IS_MS_BUILD_AVAILABLE
-fi
-
 cd "$START_DIR"
